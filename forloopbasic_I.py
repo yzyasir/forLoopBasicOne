c2ea9baeba0d3dcfd8e8e943ff799a5451134dae
Yasir Zafar <yzyasir@yahoo.com>
2020-12-24 23:26:43 -0500

    some practice for loops

---
+++ forloopbasic_I.py
@@ -0,0 +1,36 @@
+# 1) Basic - Print all integers from 0 to 150
+# for x in range(0, 151, 1):
+#     print(x)
+
+# 2) Multiples of Five - Print all the Multiples of 5 to 1000
+# for x in range(0, 1001, 5):
+#     print(x)
+
+# 3) Counting, the Dojo Way - Print integers 1 to 100. If divisible by 5, print "Coding" instead. If divisible by 10, print "Coding Dojo".
+# for x in range(1, 100, 1):
+#     if x % 5 == 0:
+#         print(Coding)
+#     if x % 10 == 0:
+#         print(Coding Dojo)
+Do this one again
+# 4) Whoa that suckers huge - Add odd integers from 0 to 500,000, and print the final sum.
+# sum = 0
+# for x in range(0, 500001, 1):
+#     if x % 2 != 0:
+#         sum = sum + x
+#         print(sum)
+
+# 5) Countdown by Fours - Print positive numbers starting at 2018, counting down by fours.
+# for x in range(2018, 2000, -4):
+#     if x > 0: 
+#         print(x)
+
+# 6) Flexible Counter 
+# lownum = 8 
+# highnum = 320
+# mult = 8
+
+# for i in range(lownum, highnum, 1):
+#     if i % mult == 0:
+#         print(i)
+Do this one again
